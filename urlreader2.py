ab21b0fc0cfb71443a84e819b4de43c7613d254e
abrosimov-d <153320622+abrosimov-d@users.noreply.github.com>
2025-01-09 18:41:59 +0300

    fix: f'' fix

--- urlreader2.py
+++ urlreader2.py
@@ -5,7 +5,7 @@ class UrlReader2:
     def __init__(self):
         self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
         opts = Options()
-        opts.add_argument(f"user-agent={self.user_agent}")
+        opts.add_argument("user-agent="+self.user_agent)
         opts.add_argument("--disable-blink-features=AutomationControlled")
         self.driver = webdriver.Chrome(options=opts)
 
